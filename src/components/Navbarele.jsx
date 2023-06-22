556f23d1d21967393a2e0721a09c0c63238062dc
SAS-Play <137288136+SAS-Play@users.noreply.github.com>
2023-06-22 18:20:17 +0530

    Update Navbarele.jsx

--- src/components/Navbarele.jsx
+++ src/components/Navbarele.jsx
@@ -34,7 +34,7 @@ const Navbarele = () => {
               <a style={{color:"cyan"}} className="nav-link active" aria-current="page" href="#devs">Devs</a>
             </li>
             <li style={{marginLeft:"5rem",marginRight:"2rem"}} className="nav-item">
-              <a style={{color:"#00ff1f"}} className="nav-link active" target='_blank' aria-current="page" href="https://drive.google.com/drive/folders/1XPuNE2kGJpZSviOpVAp77hp7GiVL4gNr?usp=sharing">Download</a>
+              <a style={{color:"#00ff1f"}} className="nav-link active" target='_blank' aria-current="page" href="https://www.dropbox.com/scl/fo/8ljc59yz80gn3dyqkwzaw/h?dl=0&rlkey=53t5hj3gar4g4zzhnwh6l4ykf">Download</a>
             </li>
             <li style={{marginLeft:"5rem",marginRight:"2rem"}} className="nav-item">
               <a style={{color:"beige"}} className="nav-link active" aria-current="page" href="#contact">Contact</a>
